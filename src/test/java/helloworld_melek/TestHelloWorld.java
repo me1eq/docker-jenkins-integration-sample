7a84ee7e5621d9a5e824d2bab5bed6cba43c6102
me1eq <melek-ozturk@hotmail.de>
2022-02-23 18:40:21 +0100

    first commit

---
+++ src/test/java/helloworld_melek/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld_melek;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("..Hello World Melek...!");
+	}
+
+}
